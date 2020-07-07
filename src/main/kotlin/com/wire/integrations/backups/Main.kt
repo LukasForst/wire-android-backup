7c7edfa6973d62812c63e2d03c3187c44e9f9f3b
Lukas Forst <lukas@forst.pw>
2020-07-07 15:25:59 +0200

    move assets, do not compile them

--- src/main/kotlin/com/wire/integrations/backups/Main.kt
+++ src/main/kotlin/com/wire/integrations/backups/Main.kt
@@ -8,13 +8,10 @@ fun main() {
     addLibraryPath("/Users/lukas/work/wire/android-db-decryption/libs")
     initSodium()
 
-    val db = File("/Users/lukas/work/wire/android-db-decryption/src/main/resources/backup.android_wbu")
+    val db = File("/Users/lukas/work/wire/android-db-decryption/testing-assets/backup.android_wbu")
     val userId = UUID.fromString("199a7b4b-4342-4de6-b4cb-f39852ee445b")
     val password = "a".toByteArray()
     val decrypted = decryptDatabase(db, password, userId) ?: return
 
     extractBackup(decrypted, userId, "/Users/lukas/work/wire/android-db-decryption")
-
-    print("YES")
 }
-//-Djava.library.path=libs
