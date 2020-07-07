ca62073a68bf15737b2a7ada13c4f9b33ebbac2c
Lukas Forst <lukas@forst.pw>
2020-07-07 15:24:37 +0200

    refactoring

--- src/main/kotlin/com/wire/integrations/backups/utils/LibLoading.kt
+++ src/main/kotlin/com/wire/integrations/backups/utils/LibLoading.kt
@@ -5,25 +5,22 @@ package com.wire.integrations.backups.utils
  * Adds the specified path to the java library path
  *
  * @param pathToAdd the path to add
- * @throws Exception
  */
-@Suppress("UNCHECKED_CAST") // it is safe as usr
 fun addLibraryPath(pathToAdd: String) {
     val usrPathsField = ClassLoader::class.java.getDeclaredField("usr_paths")
-    usrPathsField.isAccessible = true
+        .apply { isAccessible = true }
 
-    //get array of paths
-    val paths: Array<String> = usrPathsField.get(null) as Array<String>
+    // get array of paths
+    @Suppress("UNCHECKED_CAST") // it is safe as we know that this field contains array of strings
+    val paths = usrPathsField.get(null) as Array<String>
 
-    //check if the path to add is already present
-    for (path in paths) {
-        if (path == pathToAdd) {
-            return
-        }
-    }
+    // check if the path to add is already present
+    if(paths.contains(pathToAdd)) return
 
-    //add the new path
-    val newPaths = paths.copyOf(paths.size + 1)
-    newPaths[newPaths.size - 1] = pathToAdd
+    // add the new path
+    val newPaths = paths + pathToAdd
     usrPathsField.set(null, newPaths)
+
+    // set property
+    System.setProperty("java.library.path", newPaths.joinToString(":"))
 }
