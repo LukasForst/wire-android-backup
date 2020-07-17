3c862af8dd6565312740d5d0e8229ddfb8d94476
Lukas Forst <lukas@forst.pw>
2020-07-17 16:59:55 +0200

    refactoring

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 group = "pw.forst.wire.android.extraction"
-version = "1.9.915"
+version = "1.9.917"
 
 
 repositories {
--- src/main/kotlin/pw/forst/wire/android/backups/database/converters/Conversations.kt
+++ src/main/kotlin/pw/forst/wire/android/backups/database/converters/Conversations.kt
@@ -3,7 +3,6 @@
 package pw.forst.wire.android.backups.database.converters
 
 import org.jetbrains.exposed.sql.Transaction
-import org.jetbrains.exposed.sql.and
 import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.selectAll
 import pw.forst.wire.android.backups.database.dto.ConversationAddMemberDto
@@ -18,60 +17,50 @@ import pw.forst.wire.android.backups.database.model.Messages
 import java.util.UUID
 
 
-fun Transaction.getNamedConversations() =
-    Conversations
-        .slice(Conversations.id, Conversations.name)
-        .select { Conversations.name.isNotNull() }
-        .map {
-            NamedConversationDto(
-                it[Conversations.id].toUuid(),
-                it[Conversations.name] ?: "no name"
-            )
-        }
+fun Transaction.getNamedConversations() = Conversations
+    .slice(Conversations.id, Conversations.name)
+    .select { Conversations.name.isNotNull() }
+    .map {
+        NamedConversationDto(
+            it[Conversations.id].toUuid(),
+            it[Conversations.name] ?: "no name"
+        )
+    }
 
-fun Transaction.getDirectMessages(myId: UUID) =
-    (Conversations leftJoin ConversationMembers)
-        .slice(Conversations.id, Conversations.name, ConversationMembers.userId, ConversationMembers.conversationId)
-        .select {
-            (Conversations.name.isNull()) and
-                    (Conversations.id eq ConversationMembers.conversationId) and
-                    (ConversationMembers.userId neq myId.toString())
-        }.map {
-            DirectConversationDto(
-                it[Conversations.id].toUuid(),
-                it[ConversationMembers.userId].toUuid()
-            )
-        }
+fun Transaction.getDirectMessages(myId: UUID) = (Conversations leftJoin ConversationMembers)
+    .slice(Conversations.id, Conversations.name, ConversationMembers.userId, ConversationMembers.conversationId)
+    .select { Conversations.name.isNull() }
+    .groupBy({ it[Conversations.id].toUuid() }, { it[ConversationMembers.userId].toUuid() })
+    .mapValues { (_, values) -> values.firstOrNull { it != myId } ?: myId }
+    .map { (conversationId, userId) -> DirectConversationDto(conversationId, otherUser = userId) }
 
 fun Transaction.getConversationsData(): ConversationsDataDto {
-    val addMembers =
-        Messages
-            .slice(Messages.messageType, Messages.conversationId, Messages.time, Messages.userId, Messages.members)
-            .select { Messages.messageType eq "MemberJoin" }
-            .map {
-                ConversationAddMemberDto(
-                    conversationId = it[Messages.conversationId].toUuid(),
-                    timeStamp = convertToStringTime(it[Messages.time]),
-                    addingUser = it[Messages.userId].toUuid(),
-                    addedUsers = it[Messages.members]
-                        ?.split(",")
-                        ?.map(String::toUuid)
+    val addMembers = Messages
+        .slice(Messages.messageType, Messages.conversationId, Messages.time, Messages.userId, Messages.members)
+        .select { Messages.messageType eq "MemberJoin" }
+        .map {
+            ConversationAddMemberDto(
+                conversationId = it[Messages.conversationId].toUuid(),
+                timeStamp = convertToStringTime(it[Messages.time]),
+                addingUser = it[Messages.userId].toUuid(),
+                addedUsers = it[Messages.members]
+                    ?.split(",")
+                    ?.map(String::toUuid)
                         ?: emptyList()
                 )
             }
 
-    val leavingMembers =
-        Messages
-            .slice(Messages.messageType, Messages.conversationId, Messages.time, Messages.members)
-            .select { Messages.messageType eq "MemberLeave" }
-            .map {
-                ConversationLeaveMembersDto(
-                    conversationId = it[Messages.conversationId].toUuid(),
-                    timeStamp = convertToStringTime(it[Messages.time]),
-                    leavingMembers = it[Messages.members]
-                        ?.split(",")
-                        ?.map(String::toUuid)
-                        ?: emptyList()
+    val leavingMembers = Messages
+        .slice(Messages.messageType, Messages.conversationId, Messages.time, Messages.members)
+        .select { Messages.messageType eq "MemberLeave" }
+        .map {
+            ConversationLeaveMembersDto(
+                conversationId = it[Messages.conversationId].toUuid(),
+                timeStamp = convertToStringTime(it[Messages.time]),
+                leavingMembers = it[Messages.members]
+                    ?.split(",")
+                    ?.map(String::toUuid)
+                    ?: emptyList()
                 )
             }
 
