1fa1a5e9b0805397c11c07a446b77dd5c159f160
Lukas Forst <lukas@forst.pw>
2020-07-30 12:58:00 +0200

    fix edge case where asset does not have message id

--- src/test/kotlin/pw/forst/wire/backups/android/DecryptionTest.kt
+++ src/test/kotlin/pw/forst/wire/backups/android/DecryptionTest.kt
@@ -14,13 +14,28 @@ import kotlin.test.assertNotNull
 class DecryptionTest {
 
     @Test
-    fun `test decryption`() {
+    fun `test decryption maciek`() {
         addLibraryPath("libs")
         initSodium()
 
         val db = File("ignored-assets/Wire-maciek102-Backup_20200708.android_wbu")
         val userId = UUID.fromString("2f9e89c9-78a7-477d-8def-fbd7ca3846b5")
         val password = "Qwerty1!".toByteArray()
+        val decrypted = decryptDatabase(db, password, userId) ?: return
+
+        val (metadata, file) = extractBackup(decrypted, userId, ".")
+        assertNotNull(file)
+        print(metadata)
+    }
+
+    @Test
+    fun `test decryption antje`() {
+        addLibraryPath("libs")
+        initSodium()
+
+        val db = File("ignored-assets/Wire-antje5-Backup_20200723.android_wbu")
+        val userId = UUID.fromString("16dd24f3-664d-4a9f-abea-008e25a9f1a1")
+        val password = "Wire12345!".toByteArray()
         val decrypted = decryptDatabase(db, password, userId) ?: return
 
         val (metadata, file) = extractBackup(decrypted, userId, ".")
