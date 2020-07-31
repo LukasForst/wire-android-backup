2ec52494cd551b38908a1d03ca3371ee4afaa9b8
Lukas Forst <lukas@forst.pw>
2020-07-31 11:21:17 +0200

    add better testing

--- src/test/kotlin/pw/forst/wire/backups/ios/ApiKtTest.kt
+++ src/test/kotlin/pw/forst/wire/backups/ios/ApiKtTest.kt
@@ -9,7 +9,7 @@ import kotlin.test.assertTrue
 @Disabled
 class ApiKtTest {
 
-    private fun runTest(
+    private fun runPassingTest(
         db: String, userId: String, password: String,
         name: String = "test",
         outputDirectory: String = "ignored-assets/$name"
@@ -24,9 +24,21 @@ class ApiKtTest {
         println(database.messages.size)
     }
 
+    private fun runUnsupportedVersionTest(
+        db: String, userId: String, password: String,
+        name: String = "test",
+        outputDirectory: String = "ignored-assets/$name"
+    ) {
+        val ex = Assertions.assertThrows(IllegalArgumentException::class.java) {
+            runPassingTest(db, userId, password, name, outputDirectory)
+        }
+        assertTrue { ex.message!!.startsWith("Unsupported version of export!") }
+
+    }
+
     @Test
     fun `test dejan process backup`() {
-        runTest(
+        runPassingTest(
             db = "ignored-assets/dejan.ios_wbu",
             userId = "a106fcd5-3146-4551-a870-9b13b125f376",
             password = "Qwerty123!",
@@ -36,7 +48,7 @@ class ApiKtTest {
 
     @Test
     fun `test eva process backup`() {
-        runTest(
+        runPassingTest(
             db = "/Users/lukas/work/wire/android-db-decryption/ignored-assets/ios_backup.ios_wbu",
             userId = "e4d71ce0-eb3a-48f6-b319-d677a2dd23b1",
             password = "Aa12345!",
@@ -46,7 +58,7 @@ class ApiKtTest {
 
     @Test
     fun `test dejan56 process backup`() {
-        runTest(
+        runPassingTest(
             db = "/Users/lukas/work/wire/android-db-decryption/ignored-assets/dejan56.ios_wbu",
             userId = "a106fcd5-3146-4551-a870-9b13b125f376",
             password = "Qwerty123!",
@@ -56,15 +68,11 @@ class ApiKtTest {
 
     @Test
     fun `test berlinerzeitung process backup`() {
-        // the kotlin version for some reason does not work
-        val ex = Assertions.assertThrows(IllegalArgumentException::class.java) {
-            runTest(
-                db = "/Users/lukas/work/wire/android-db-decryption/ignored-assets/Wire-berlinerzeitung-Backup_20200730.ios_wbu",
-                userId = "2bc78bf0-86ee-4701-9713-245216a60df9",
-                password = "BackupFile1!",
-                name = "berlinerzeitung"
-            )
-        }
-        assertTrue { ex.message!!.startsWith("Unsupported version of export!") }
+        runUnsupportedVersionTest(
+            db = "/Users/lukas/work/wire/android-db-decryption/ignored-assets/Wire-berlinerzeitung-Backup_20200730.ios_wbu",
+            userId = "2bc78bf0-86ee-4701-9713-245216a60df9",
+            password = "BackupFile1!",
+            name = "berlinerzeitung"
+        )
     }
 }
