f3764ca7f61435b8d0714dac6311c6cd373b73b7
Lukas Forst <lukas@forst.pw>
2020-08-04 11:57:34 +0200

    explicitly ask for loading library from system with fallback to bundled versions

--- src/main/kotlin/pw/forst/wire/backups/android/steps/DatabaseDecrypt.kt
+++ src/main/kotlin/pw/forst/wire/backups/android/steps/DatabaseDecrypt.kt
@@ -1,6 +1,7 @@
 package pw.forst.wire.backups.android.steps
 
 import com.goterl.lazycode.lazysodium.SodiumJava
+import com.goterl.lazycode.lazysodium.utils.LibraryLoader
 import java.io.File
 import java.util.UUID
 
@@ -11,7 +12,7 @@ fun decryptDatabase(databaseFile: File, password: ByteArray, userId: UUID): File
     // read metadata
     val metadata = readEncryptedMetadata(databaseFile)
     // init sodium lib
-    val sodium = SodiumJava()
+    val sodium = SodiumJava(LibraryLoader.Mode.PREFER_SYSTEM)
 
     val hash = sodium.hash(userId.toString().toByteArray(), metadata.salt, metadata.opslimit, metadata.memlimit)
     require(hash.contentEquals(metadata.uuidHash)) { "Uuid hashes don't match" }
--- src/main/kotlin/pw/forst/wire/backups/ios/decryption/Decrypt.kt
+++ src/main/kotlin/pw/forst/wire/backups/ios/decryption/Decrypt.kt
@@ -2,6 +2,7 @@ package pw.forst.wire.backups.ios.decryption
 
 import com.goterl.lazycode.lazysodium.SodiumJava
 import com.goterl.lazycode.lazysodium.interfaces.SecretStream
+import com.goterl.lazycode.lazysodium.utils.LibraryLoader
 import java.io.File
 import java.nio.ByteBuffer
 import java.util.UUID
@@ -18,7 +19,7 @@ fun decryptIosBackup(databaseFile: File, password: String, userId: UUID): ByteAr
  */
 fun decryptIosBackup(input: ByteBuffer, password: String, userId: UUID): ByteArray {
     // load Sodium library
-    val sodium = SodiumJava()
+    val sodium = SodiumJava(LibraryLoader.Mode.PREFER_SYSTEM)
     // read and verifies the header, modifies input buffer
     val fileHeader = readHeader(input)
     // now we check the uuid
